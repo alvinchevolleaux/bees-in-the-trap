0291e1aeadb4351c1e017e66a3b517b7f87967e0
Alvin Chevolleaux <alvin@chevolleaux.com>
2020-01-27 23:51:47 +0000

    Progress with game mechanics

--- src/Domain/Game.php
+++ src/Domain/Game.php
@@ -1,8 +1,10 @@
 <?php
 
+declare(strict_types=1);
 
 namespace BeesInTheTrap\Domain;
 
+use Zend\Math\Rand;
 
 class Game
 {
@@ -14,6 +16,17 @@ class Game
     }
 
     public function takeTurn() {
+        $bee = $this->getRandomBee();
+    }
 
+    private function getRandomLivingBee() : Bee {
+        do {
+            $bees = $this->hive->getBees();
+            $beeNumber = Rand::getInteger(0, count($bees));
+            /** @var Bee $bee */
+            $bee = $bees[$beeNumber];
+        } while (!$bee->isAlive());
+
+        return $bee;
     }
 }
